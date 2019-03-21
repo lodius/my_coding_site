0d0c7d4245e30ae36f30bbb8477a13b0b07982cc
lodius <quakehoang091194@gmail.com>
2019-03-21 17:18:56 +0900

    Initial commit. Setting up the project for success.

--- my_coding_site/settings.py
+++ my_coding_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@=0bh8a-grn!*8zkkboive9fh)_=xvv_f)n1zwbouzl_^1f81='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Japan'
 
 USE_I18N = True
 
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
